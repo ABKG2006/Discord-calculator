8d048cda3492f1f128f670a9ccbb13d96df7844f
ABKG2006 <144052988+ABKG2006@users.noreply.github.com>
2023-10-09 00:15:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
+import os
+import random
 import discord 
 from discord.ext import commands
 from logic import *
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='//', intents=intents)
 
 
 @bot.event
@@ -17,5 +18,19 @@ async def calc(ctx, a):
     await ctx.send(calculate(a))
 @bot.command()
 async def oplist(ctx):
-    await ctx.send("Avaliable opperations:\nAdddition(x+y)\nSubtraction(x-y)\nMultiplication(x*y)\nDivision(x/y)\nInteger division(x//y)\nModulo(x%y)\nExponantiation(x**y or x^y)\n\n***Note: All trig functions use radians***\n\nconvert from degrees ( x deg )\nsine( sin(x) )\ncosine( cos(x) )\ntangent( tan(x) )\ncotangent( cot(x) )\nsecant( sec(x) )\ncosecant( cosec(x) )\n\nConstants:\npi=3.141...\ne=2.718...",ephemeral=True)
-bot.run("TOKEN buraya/here")
+    await ctx.send("Avaliable opperations:\nAdddition(x+y)\nSubtraction(x-y)\nMultiplication(x*y)\nDivision(x/y)\nInteger division(x//y)\nModulo(x%y)\nExponantiation(x**y or x^y)\nSquare root(sqrt(x))\n\n***Note: All trig functions use radians***\n\nconvert from degrees ( x deg )\nsine( sin(x) )\ncosine( cos(x) )\ntangent( tan(x) )\ncotangent( cot(x) )\nsecant( sec(x) )\ncosecant( cosec(x) )\n\nConstants:\npi=3.141...\ne=2.718...\ni=sqrt(-1)",ephemeral=True)
+@bot.command()
+async def mem(ctx):
+    try:
+        image=random.choice(os.listdir('images'))
+        with open(f'images/{image}', 'rb') as f:
+            picture = discord.File(f)
+        await ctx.send(file=picture)
+    except:
+        await ctx.send("Unable to get memes at the moment,sorry")    
+@bot.command()
+async def fox(ctx):
+    image_url = get_foxxo()
+    await ctx.send(image_url)
+bot.run("token go here")
+
