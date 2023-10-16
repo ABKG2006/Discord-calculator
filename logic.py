8a13c929df0c24f325eb8b0fdb05ecdf7b130d6a
ABKG2006 <144052988+ABKG2006@users.noreply.github.com>
2023-10-16 19:01:05 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -2,25 +2,21 @@ import requests
 i=1j
 e=2.718281828459045
 pi=3.141592653589793
-omited_chars="wyuığüjadfghklşzxvbmöçQWERTYUIOPĞÜİŞLKJHGFDSA!?_"
-clean_dict={"^":"**","deg":"*pi/180","1j":"i","j":"i"}
+clean_dict={"^":"**","deg":"*pi/180","1j":"i","j":"i","print":"",":":"/","send":"","import":""}
 def calculate(express):
-    if omited_chars in str(express):
-        return "Invalid chars in expression"
-    else:
-        try:
-            x=eval(clean(express))
-            if x.imag!=0: 
-                if x.real!=0:
-                    x=clean(str(x))
-                    return x[1:-1]
-                else:
-                    x=clean(str(x))
-                    return x
+    try:
+        x=eval(clean(express))
+        if x.imag!=0: 
+            if x.real!=0:
+                x=clean(str(x))
+                return x[1:-1]
             else:
+                x=clean(str(x))
+                return x
+        else:
                 return str(x.real)
-        except:
-            return "invalid input"
+    except:
+        return "invalid input"
 
 
         
@@ -32,31 +28,37 @@ def taylorsin(a):
 def taylorcos(a):
     return 1-a**2/2+a**4/24-a**6/720+a**8/40320-a**10/3628800+a**12/479001600
 def sin(a):
-    a=a%(2*pi)
-    region=a//(pi/2)
-    a=a%(pi/2)
-    match region:
-        case 0:
-            return taylorsin(a)
-        case 1:
-            return taylorcos(a)
-        case 2:
-            return -taylorsin(a)
-        case 3:
-            return -taylorcos(a)
+    if a.imag==0:
+        a=a%(2*pi)
+        region=a//(pi/2)
+        a=a%(pi/2)
+        match region:
+            case 0:
+                return taylorsin(a)
+            case 1:
+                return taylorcos(a)
+            case 2:
+                return -taylorsin(a)
+            case 3:
+                return -taylorcos(a)
+    else:
+        return (e**(i*a)-e**(i*-a))/(2*i)
 def cos(a):
-    a=a%(2*pi)
-    region=a//(pi/2)
-    a=a%(pi/2)
-    match region:
-        case 0:
-            return taylorcos(a)
-        case 1:
-            return -taylorsin(a)
-        case 2:
-            return -taylorcos(a)
-        case 3:
-            return taylorsin(a)
+    if a.imag==0:
+        a=a%(2*pi)
+        region=a.real//(pi/2)
+        a=a%(pi/2)
+        match region:
+            case 0:
+                return taylorcos(a)
+            case 1:
+                return -taylorsin(a)
+            case 2:
+                return -taylorcos(a)
+            case 3:
+                return taylorsin(a)
+    else:
+        return (e**(i*a)+e**(i*-a))/2
 def tan(a):
     return sin(a)/cos(a)
 def cot(a):
@@ -72,4 +74,4 @@ def clean(a):
 def get_foxxo():
     res = requests.get('https://randomfox.ca/floof')
     data = res.json()
-    return data['link']
+    return data['image']
